448173b436cac23120daf2798013fca57df854c9
Jan Seidel <118558932+JOOHNYS@users.noreply.github.com>
2023-04-19 02:10:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from machine_learning import MachineLearning
 
 if __name__ == "__main__":
 
-    print("Program do przeprowadzenia eksperymentów porównawczych SVM z metodami referencyjnymi.")
-    print("Autorzy: Seidel Jan, Gincel Lukasz")
+    print("Program do przeprowadzenia eksperymentów porównawczych support vector machines z metodami referencyjnymi.")
+    print("Autorzy: J. Seidel, Ł. Gincel\n")
 
     ml = MachineLearning()
     ml.run_experiments()
